6d605322e3996df286534db5efd64710e67155e4
shubham0219 <65565631+shubham0219@users.noreply.github.com>
2020-10-18 12:38:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+  count: number = 0;
+
+  // increaseCount(){
+
+  // }
+
+  increaseCount = () => {
+    //increaseCount not greater then 10
+    if (this.count === 10) {
+      return this.count;
+    }
+    this.count = this.count + 1;
+  };
+
+  decreaseCount = () => {
+    //decreaseCount no negative number
+    if (this.count <= 0) {
+      return this.count;
+    }
+    this.count = this.count - 1;
+  };
+
+  resetCount = () => {
+    this.count = 0;
+  };
 }
